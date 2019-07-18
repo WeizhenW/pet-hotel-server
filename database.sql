86e51a00a6528faaeda0673e46122869eaba4d4e
Kyle Henderson <kthenderson83@gmail.com>
2019-07-18 14:14:30 -0500

    insert owners and pets into db

--- database.sql
+++ database.sql
@@ -1,5 +1,10 @@
 --db_name = pet_hotel
 
+--DROP TABLE "owner";
+
+SELECT * FROM "owner";
+SELECT * FROM "pet";
+
 CREATE TABLE "owner" (
 "id" SERIAL PRIMARY KEY,
 "name" VARCHAR(80),
@@ -24,3 +29,16 @@ CREATE TABLE "visit" (
 "checkin_date" DATE DEFAULT CURRENT_DATE,
 "checkout_date" DATE
 );
+
+INSERT INTO "owner" (name, address, phone) VALUES 
+('Sue', '123 Something rd', '5551234567'),
+('Jenny Jenny', '12 Springfield Ln', '5558675309'),
+('Jack', '201 Melloncamp St', '1234567890'),
+('Diane', '402 Melloncamp St', '1098765432');
+
+INSERT INTO "pet" (name, breed, color, owner_id, check_in, age) VALUES
+('Charlie', 'shih-tzu', 'black', 1, false, 3),
+('Thorin', 'rabbit', 'white', 2, false, 2),
+('Gatsby', 'cat', 'white', 3, false, 7),
+('Angus', 'scottie dog', 'black', 4, false, 6),
+('Bear', 'dog', 'black', 4, false, 10);
